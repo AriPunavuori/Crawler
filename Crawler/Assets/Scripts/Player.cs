c81344f0baacc62c42b656215bd5813bf88ee1c5
JuhisRasis <47595899+JuhisRasis@users.noreply.github.com>
2020-03-18 14:40:41 +0200

    Nyt pitäis olla kunnossa

--- Crawler/Assets/Scripts/Player.cs
+++ Crawler/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Photon.MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rigidBody;
@@ -14,22 +14,36 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+
         rigidBody = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
+        if (photonView.isMine){
         if (Input.GetKeyDown(KeyCode.Mouse0)){
-            GameObject BoltSpawnInstance = Instantiate(bolt, boltSpawn.transform.position, Quaternion.identity);
-            BoltSpawnInstance.transform.SetParent(MagicalGirlJoint.transform);
-            BoltSpawnInstance.GetComponent<Rigidbody2D>().AddForce(MagicalGirlJoint.transform.right * boltForce, ForceMode2D.Impulse);
+                Shoot();
+                photonView.RPC("Shoot", PhotonTargets.All);
         }
     }
+ }
 
     void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
+        if (photonView.isMine)
+        {
+
+            float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         rigidBody.velocity = new Vector2(x * speed, y * speed);
 
+        }
     }
+    [PunRPC]
+    public void Shoot()
+    {
+        GameObject BoltSpawnInstance = Instantiate(bolt, boltSpawn.transform.position, Quaternion.identity);
+        BoltSpawnInstance.transform.SetParent(MagicalGirlJoint.transform);
+        BoltSpawnInstance.GetComponent<Rigidbody2D>().AddForce(MagicalGirlJoint.transform.right * boltForce, ForceMode2D.Impulse);
+    }
+
 }
--- Crawler/Assets/Scripts/PlayerFacingAt.cs
+++ Crawler/Assets/Scripts/PlayerFacingAt.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerFacingAt : MonoBehaviour
+public class PlayerFacingAt : Photon.MonoBehaviour
 {
+    public Camera myCam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (photonView.isMine)
+        {
+            myCam.enabled = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (photonView.isMine)
+        {
+            if (Camera.main != null)
+            {
+                Debug.Log("fucg");
+            }
+
             Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
             Vector3 dir = Input.mousePosition - pos;
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
     }
+}
 
