5319a5a468ed147eab3e83fc51a80d2e3791d6b9
Ari Punavuori <ari.punavuori@gmail.com>
2020-03-18 21:36:08 +0200

    Class inheritance test, EnemyAI demo
    PlayerCharacter and EnemyCharacter classes that inherit Character class(Health and Damage variables just for show). Small NavMesh Demo (Enemy follows Player).

---
+++ Crawler/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Vector3 camOffset;
+    Transform player;
+    void Start() {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var pPos = player.position;
+        transform.position = new Vector3(pPos.x + camOffset.x, pPos.y + camOffset.y, pPos.z + camOffset.z);
+    }
+}
---
+++ Crawler/Assets/Scripts/EnemyCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyCharacter : Character {
+    NavMeshAgent agent;
+    Transform player;
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update() {
+        var pPos = player.position;
+        agent.destination = pPos;
+    }
+}
