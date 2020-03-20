504ddf8f9cc26b8a37e3346546dfea520b0218d1
Emil Virolainen <virolainen.emil@gmail.com>
2020-03-20 12:36:05 +0200

    Puoliksi toimiva Enemy AI

---
+++ Crawler/Assets/Scripts/EmilEnemyCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmilEnemyCharacter : Character {
+
+	public GameObject player;
+
+	void Update() {
+
+		if (GameObject.Find("MagicalGirl(Clone)") != null && player == null) {
+			player = GameObject.Find("MagicalGirl(Clone)");
+		}
+
+		if (player != null) {
+			float dist = Vector2.Distance(transform.position, player.transform.position);
+			Debug.DrawLine(transform.position, player.transform.position, Color.red);
+
+			if (!Physics.Linecast(transform.position, player.transform.position) && dist < 7) { //Välissä ei esteitä ja etäisyys on < x
+				Vector3 moveDir = (player.transform.position - transform.position).normalized;
+				transform.position += moveDir * speed * Time.deltaTime;
+				//Seurataan pelaajaa (liikutaan pelaajan suuntaan)
+			} else {
+				//On este, ei voi seurata.
+			}
+		}
+	}
+}
