e430bec24de17eb9f29e723d4f445e22fb537681
Ari Punavuori <ari.punavuori@gmail.com>
2020-03-20 10:44:36 +0200

    Character, PlayerCharacter

--- Crawler/Assets/Scripts/Character.cs
+++ Crawler/Assets/Scripts/Character.cs
@@ -6,6 +6,7 @@ public class Character : MonoBehaviour {
     public bool ranged;
     public int damage;
     public int attackRange;
+    public float attackAngle;
     public float attackInterval;
     public float speed;
     public int health;
--- Crawler/Assets/Scripts/PlayerCharacter.cs
+++ Crawler/Assets/Scripts/PlayerCharacter.cs
@@ -18,7 +18,6 @@ public class PlayerCharacter : Character {
         }
     }
 
-
     void Update() {
 
     }
