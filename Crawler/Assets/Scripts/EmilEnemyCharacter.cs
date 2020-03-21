8be55fb37d2eb1947c66d685faaaf5ad70b81cf0
Emil Virolainen <virolainen.emil@gmail.com>
2020-03-21 19:29:36 +0200

    Enemy AI ottaa huomioon kaikki pelaajat (multiplayer)

--- Crawler/Assets/Scripts/EmilEnemyCharacter.cs
+++ Crawler/Assets/Scripts/EmilEnemyCharacter.cs
@@ -4,61 +4,47 @@ using UnityEngine;
 
 public class EmilEnemyCharacter : Character {
 
-    private Rigidbody2D rigidBody;
-    LayerMask layerMask;
-    public GameObject player;
-    Vector3 target;
-    float targetDistance = 1.25f;
-    float detectionDistance = 5f;
-    void Start() {
-        layerMask = LayerMask.GetMask("Player", "Obstacles");
-        rigidBody = GetComponent<Rigidbody2D>();
-    }
+	private Rigidbody2D rigidBody;
+	LayerMask layerMask;
+	public GameObject player;
+	Vector3 target;
+	float targetDistance = 1.25f;
+	float detectionDistance = 5f;
+	public Collider2D[] players;
+	bool following;
 
-    void FixedUpdate() {
+	void Start() {
+		layerMask = LayerMask.GetMask("Player", "Obstacles");
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
 
-        if(player == null) {
-            player = GameObject.Find("MagicalGirl(Clone)");
-        } else {
-            var dirVector = player.transform.position - transform.position;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, dirVector, detectionDistance, layerMask); 
-            //Debug.Log(hit.collider);
-            rigidBody.velocity = Vector3.zero;
+	void FixedUpdate() {
 
-            if(hit.collider != null && Vector3.Distance(transform.position, player.transform.position) < detectionDistance) {
-                Debug.DrawLine(transform.position, hit.point, Color.red);
-                target = hit.point;
-            }
-        }
-        if(Vector3.Distance(transform.position, target) > targetDistance) {
-            Vector3 moveDir = (target - transform.position).normalized;
-            rigidBody.velocity = moveDir * speed;
-        }
-    }
+		if (player == null) { // Jos ei jahdattavaa
+			players = Physics2D.OverlapCircleAll(transform.position, detectionDistance, LayerMask.GetMask("Player")); //Etsi 2Dcollidereita detectionDistance-kokoiselta, ympyrän muotoiselta alueelta
+			if (players.Length > 0) { // Jos löytyi
+				player = players[0].gameObject; // Aseta ensimmäinen löytynyt jahdattavaksi
+				following = true;
+			}
+		} else { // Jos on jahdattava
+			Vector2 dirVector = player.transform.position - transform.position; // Pelaajan suuntaan vihollisesta
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, dirVector, detectionDistance, layerMask); // Castataan ray pelaajaan
+			rigidBody.velocity = Vector3.zero; // Pysähdytään
+
+			if (hit.collider != null) { // Jos ray osui pelaajaan/seinään
+				if (Vector3.Distance(transform.position, player.transform.position) < detectionDistance) { // Jos etäisyys jahdattavaan < 5
+					Debug.DrawLine(transform.position, hit.point, Color.red); // Piirrä punanen debug viiva
+					target = hit.point; // Jahdattavan objektin olinpaikka
+				} else { // Jos meni liian kauas
+					player = null; // Ei jahdattavaa
+					following = false;
+				}
+			}
+
+			if (Vector3.Distance(transform.position, target) > targetDistance && following) { // Jos etäisyys jahdattavan olinpaikkaan > targetDistance
+				Vector3 moveDir = (target - transform.position).normalized; // Suunta jahdattavaa päin
+				rigidBody.velocity = moveDir * speed; // liiku jahdattavan suuntaan
+			}
+		}
+	}
 }
-
-
-
-//float dist = Vector2.Distance(transform.position, player.transform.position);
-//Debug.DrawLine(transform.position, player.transform.position, Color.red);
-
-//Vector2 pPositionVec2 = player.transform.position;
-//Vector2 ePositionVec2 = transform.position;
-
-//Debug.Log(pPositionVec2);
-//Debug.Log(ePositionVec2);
-//RaycastHit2D hit = Physics2D.Linecast(ePositionVec2, pPositionVec2);
-//Debug.Log(hit.collider);
-//rigidBody.velocity = Vector3.zero;
-
-////if (!Physics.Linecast(transform.position, player.transform.position) && dist < 7) { //Välissä ei esteitä ja etäisyys on < x
-//if(hit.collider != null && hit.collider.tag != "Wall" && dist < 7) {
-//    Vector3 moveDir = (player.transform.position - transform.position).normalized;
-//    rigidBody.velocity = moveDir * speed;
-
-//    //transform.position += moveDir * speed * Time.deltaTime;
-//    //Seurataan pelaajaa (liikutaan pelaajan suuntaan)
-//} else {
-//    //On este, ei voi seurata.
-//    Debug.Log("ei näie");
-//}
