2ba5a8ad7c2e369a5b6c927c340c31a75a20835c
NegDevv <62241375+NegDevv@users.noreply.github.com>
2020-03-25 13:31:02 +0200

    Dashing and magiGirl animation
    - PlayerCharacter script now includes dashing
    - NetworkPlayer is now animated

--- Crawler/Assets/Scripts/PlayerCharacter.cs
+++ Crawler/Assets/Scripts/PlayerCharacter.cs
@@ -7,9 +7,19 @@ public class PlayerCharacter : Character {
 
     Rigidbody2D rb2D;
     bool potion;
+    bool dashing = false;
+    float dashCooldown = 5.0f;
+    float dashTime = 0.15f;
+    float dashTimer = 0.1f;
+    float dashFactor = 4.0f;
+    Vector2 dashVector;
+    Vector2 lastDir;
+    public Animator animator;
 
     void Start() {
+        
         rb2D = GetComponent<Rigidbody2D>();
+        dashCooldown = 0.0f;
 
         if(characterType == EntityType.Hero0) {
             ranged = true;
@@ -55,18 +65,89 @@ public class PlayerCharacter : Character {
             // Movement input
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
+
+            
+            if(movement.x != 0 || movement.y != 0)
+            {
+                lastDir = new Vector2(movement.x, movement.y);
+            }
+
+
+            // Setting the correct animation/stance depending on the current mouse position and if moving or not
+            Vector3 mousePos = Camera.main.WorldToScreenPoint(transform.position);
+            Vector2 mouseVector = new Vector2(Input.mousePosition.x - mousePos.x, Input.mousePosition.y - mousePos.y);
+            
+            animator.SetFloat("Horizontal", mouseVector.x);
+            animator.SetFloat("Vertical", mouseVector.y);
+            animator.SetFloat("Magnitude", movement.magnitude);
+
+
+
+            if (Input.GetKeyDown(KeyCode.Space) && dashCooldown <= 0)
+            {
+                Debug.Log("Dashing");
+                dashing = true;
+                dashCooldown = 5.0f;
+
+                // Initial dash direction from mouse position
+                //Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
+                //dashVector = new Vector2(Input.mousePosition.x - position.x, Input.mousePosition.y - position.y);
+
+                // Dashing to the last movement direction from keyboard inputs
+                dashVector = lastDir;
+            }
+
+            if (dashing)
+            {
+                dashTimer -= Time.deltaTime;
+
+
+                // Updating dashing direction mid dash with mouse position
+                //Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
+                //dashVector = new Vector2(Input.mousePosition.x - position.x, Input.mousePosition.y - position.y);
+
+                // Updating dashing direction mid dash with keyboard inputs. Comment to have static direction
+                //dashVector = lastDir;
+
+            }
+
+            if (dashTimer <= 0)
+            {
+                dashing = false;
+                dashTimer = dashTime;
+            }
+
+            if (!dashing && dashCooldown - Time.deltaTime > 0)
+            {
+                dashCooldown -= Time.deltaTime;
+            }
+            else if(!dashing && dashCooldown - Time.deltaTime < 0)
+            {
+                dashCooldown = 0f;
+            }
         }
+        
         if (health <= 0)
         {
             Destroy(gameObject); // Kuole
         }
+
     }
 
     private void FixedUpdate() {
         // Move the PlayerCharacter of the correct player
         if(photonView.isMine) {
             if(rb2D != null)
-                rb2D.velocity = new Vector2(movement.x * speed, movement.y * speed).normalized * speed;
+                if(dashing)
+                {
+                    //rb2D.velocity = new Vector2(movement.x * speed, movement.y * speed).normalized * speed * dashFactor;
+                    rb2D.velocity = dashVector.normalized * speed * dashFactor;
+                }
+                else
+                {
+                    rb2D.velocity = new Vector2(movement.x * speed, movement.y * speed).normalized * speed;
+                }
+                //Debug.Log(rb2D.velocity.magnitude);
         }
     }
 
--- Crawler/Assets/Scripts/PlayerFacingAt.cs
+++ Crawler/Assets/Scripts/PlayerFacingAt.cs
@@ -28,8 +28,9 @@ public class PlayerFacingAt : Photon.MonoBehaviour
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
+            
 
-            Debug.Log(transform.rotation.eulerAngles);
+            //Debug.Log(transform.rotation.eulerAngles);
             float mouseAngle = transform.rotation.eulerAngles.z;
             float angle22dot5 = 22.5f;
 
