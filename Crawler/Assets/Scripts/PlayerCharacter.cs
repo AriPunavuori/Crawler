234db446b30e695c7d5725fd836661916e92d495
NegDevv <62241375+NegDevv@users.noreply.github.com>
2020-03-26 16:25:19 +0200

    Animation sync for photon
    -Animations now sync with other players over photon
    - NetworkPlayer prefab velocity sync turned off
    - PlayerCharacter script changes rigidbody to kinematic if photonview is not own

--- Crawler/Assets/Scripts/PlayerCharacter.cs
+++ Crawler/Assets/Scripts/PlayerCharacter.cs
@@ -105,6 +105,10 @@ public class PlayerCharacter : Character {
                 dashCooldown = 0f;
             }
         }
+        else
+        {
+            rb2D.isKinematic = true;
+        }
     }
 
     private void FixedUpdate() {
