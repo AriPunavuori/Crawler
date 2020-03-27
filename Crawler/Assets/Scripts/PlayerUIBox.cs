4ad21840014fd73909acd810c69ade3d92573f4b
Emil Virolainen <virolainen.emil@gmail.com>
2020-03-27 11:35:57 +0200

    1. Versio pelaajien UI-laatikoista ruudun alareunassa.

---
+++ Crawler/Assets/Scripts/PlayerUIBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerUIBox : MonoBehaviour {
+	public GameObject player;
+	Text hpText;
+	Text nameText;
+
+	PlayerCharacter pc;
+	void Start() {
+		nameText = transform.GetChild(0).gameObject.GetComponent<Text>();
+		hpText = transform.GetChild(1).gameObject.GetComponent<Text>();
+	}
+
+	void Update() {
+
+		if (player != null) {
+			pc = player.GetComponent<PlayerCharacter>();
+			hpText.text = "HP = " + pc.health.ToString();
+			nameText.text = pc.name;
+			if (pc.characterType == EntityType.Hero0) {
+				transform.GetComponent<Image>().color = Color.blue;
+			} else if (pc.characterType == EntityType.Hero1) {
+				transform.GetComponent<Image>().color = Color.green;
+			} else if (pc.characterType == EntityType.Hero2) {
+				transform.GetComponent<Image>().color = Color.yellow;
+			} else if (pc.characterType == EntityType.Hero3) {
+				transform.GetComponent<Image>().color = Color.red;
+			}
+		}
+	}
+}
