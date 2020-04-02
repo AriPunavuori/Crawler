aac83e3f7dcd4dbd67449a8ba1232864b627dd82
Emil Virolainen <virolainen.emil@gmail.com>
2020-04-02 16:50:26 +0300

    UIBox -ongelma

--- Crawler/Assets/Scripts/GameManager.cs
+++ Crawler/Assets/Scripts/GameManager.cs
@@ -8,26 +8,31 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class GameManager : Photon.MonoBehaviour {
 	int keys = 0;
 	Canvas canvas;
-	float counter;
 	public bool useUIBoxes = true;
 	public GameObject[] playerUIBoxes;
 	public GameObject playerUIBox;
+	public GameObject emptyUIBox;
 	GameObject[] players;
+	PhotonPlayer[] photonPlayers;
+	Room currentRoom;
 	Text keysUI;
 
 	void Start() {
 		canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
 		keysUI = GameObject.Find("Keys").GetComponent<Text>();
+		currentRoom = PhotonNetwork.room;
+		photonPlayers = PhotonNetwork.playerList;
 	}
 
 	private void Update() {
 		if (useUIBoxes) {
-			if (counter >= 1) {
-				UpdatePlayerUIBoxes();
-				counter = 0;
-			} else {
-				counter += Time.deltaTime;
+			Debug.Log(photonPlayers.Length);
+
+			if (PhotonNetwork.playerList != photonPlayers || PhotonNetwork.room != currentRoom) { // Jos pelaajien määrä tai huone vaihtuu
+				UpdatePlayerUIBoxes(); // Päivitä UIBoxit
 			}
+			photonPlayers = PhotonNetwork.playerList;
+			currentRoom = PhotonNetwork.room;
 		}
 		keysUI.text = "Keys: " + keys.ToString();
 	}
@@ -66,13 +71,19 @@ public class GameManager : Photon.MonoBehaviour {
 		foreach (Transform child in canvas.transform.GetChild(0).transform) {
 			Destroy(child.gameObject);  // Tuhoa vanhat UIBoxit
 		}
-		playerUIBoxes = new GameObject[players.Length]; // Taulukon koko on playerien määrä
-		for (int i = 0; i < players.Length; i++) {
-			GameObject newUIBox = Instantiate(playerUIBox.gameObject); // Tee uusi UIBox
-			newUIBox.GetComponent<PlayerUIBox>().myPlayer = players[i]; // Anna boxin playeriksi listan i:es pelaaja
-			players[i].GetComponent<PlayerCharacter>().myUIBox = newUIBox; // Anna anna i:en pelaajan boxiksi uusi boxi
-			playerUIBoxes[i] = newUIBox;    // Aseta uusi boxi taulukkoon
-			playerUIBoxes[i].transform.SetParent(canvas.transform.GetChild(0)); // laita boxi canvasin "players" -elementin lapseksi
+		playerUIBoxes = new GameObject[4]; // Taulukon koko on aina 4
+		for (int i = 0; i < 4; i++) {
+			if (i < players.Length) {
+				GameObject newUIBox = Instantiate(playerUIBox.gameObject); // Tee uusi UIBox
+				newUIBox.GetComponent<PlayerUIBox>().myPlayer = players[i]; // Anna boxin playeriksi listan i:es pelaaja
+				players[i].GetComponent<PlayerCharacter>().myUIBox = newUIBox; // Anna i:en pelaajan boxiksi uusi boxi
+				playerUIBoxes[i] = newUIBox;    // Aseta uusi boxi taulukkoon
+				playerUIBoxes[i].transform.SetParent(canvas.transform.GetChild(0)); // laita boxi canvasin "players" -elementin lapseksi
+			} else {
+				GameObject newEmptyUIBox = Instantiate(emptyUIBox.gameObject); // Tee uusi tyhjä UIBox
+				playerUIBoxes[i] = newEmptyUIBox;    // Aseta tyhjä boxi taulukkoon
+				playerUIBoxes[i].transform.SetParent(canvas.transform.GetChild(0)); // laita tyhjä boxi canvasin "players" -elementin lapseksi
+			}
 		}
 	}
 }
