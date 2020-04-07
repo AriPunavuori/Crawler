aa049fdb06f74b3a8ffcde44db3094373f619836
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-07 11:59:00 +0300

    UI back online

--- Crawler/Assets/Scripts/PlayerUIBox.cs
+++ Crawler/Assets/Scripts/PlayerUIBox.cs
@@ -19,19 +19,19 @@ public class PlayerUIBox : MonoBehaviour {
 
 	void Update() {
 
-		//if (myPlayer != null) {
-		//	pc = myPlayer.GetComponent<PlayerCharacter>();
-		//	hpText.text = "HP = " + PlayerManager.Instance.GetHealth(photonView.owner).ToString();
-		//	nameText.text = PlayerManager.Instance.GetName(photonView.owner);
-		//	if (pc.characterType == EntityType.Hero0) {
-		//		transform.GetComponent<Image>().color = Color.blue;
-		//	} else if (pc.characterType == EntityType.Hero1) {
-		//		transform.GetComponent<Image>().color = Color.green;
-		//	} else if (pc.characterType == EntityType.Hero2) {
-		//		transform.GetComponent<Image>().color = Color.yellow;
-		//	} else if (pc.characterType == EntityType.Hero3) {
-		//		transform.GetComponent<Image>().color = Color.red;
-		//	}
-		//}
+		if(myPlayer != null) {
+			pc = myPlayer.GetComponent<PlayerCharacter>();
+			hpText.text = "HP = " + PlayerManager.Instance.GetHealth(photonView.owner).ToString();
+			nameText.text = PlayerManager.Instance.GetName(photonView.owner);
+			if(pc.characterType == EntityType.Hero0) {
+				transform.GetComponent<Image>().color = Color.blue;
+			} else if(pc.characterType == EntityType.Hero1) {
+				transform.GetComponent<Image>().color = Color.green;
+			} else if(pc.characterType == EntityType.Hero2) {
+				transform.GetComponent<Image>().color = Color.yellow;
+			} else if(pc.characterType == EntityType.Hero3) {
+				transform.GetComponent<Image>().color = Color.red;
+			}
+		}
 	}
 }
