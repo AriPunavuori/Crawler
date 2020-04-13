98932e75d2e60031591997fa121aa5a9046a0c5c
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-13 12:59:14 +0300

    Prefab update

--- Crawler/Assets/Scripts/Misc/Character.cs
+++ Crawler/Assets/Scripts/Misc/Character.cs
@@ -15,7 +15,7 @@ public class Character : Photon.MonoBehaviour {
     public int[] damages = new int[] { 5, 5, 10, 10, 5, 5, 10, 10 };  // Amount of damage
     public float[] attackIntervals = new float[] { 0.2f, 0.2f, 0.5f, 0.5f, .5f, .5f, .5f, .5f }; // Attack interval
     public float[] speeds = new float[] { 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f }; // Movement speed
-    public int[] healths = new int[] { 200, 200, 200, 200, 20 , 60, 100, 100 }; // Health
+    public int[] healths = new int[] { 200, 200, 200, 200, 40 , 60, 100, 100 }; // Health
 
     public bool npc;
     public bool ranged;
