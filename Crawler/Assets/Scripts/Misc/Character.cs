8652dbcbbcdaacdb852cedb21fbf313a341ae63d
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-13 14:04:58 +0300

    Enemy look for new player when player dies

--- Crawler/Assets/Scripts/Misc/Character.cs
+++ Crawler/Assets/Scripts/Misc/Character.cs
@@ -12,10 +12,10 @@ public class Character : Photon.MonoBehaviour {
     bool[] rangeds = new bool[] { true, true, false, false, true, true, false, false }; // Ranger or melee
     float[] projectileSpeeds = new float[] { 10, 10, 10, 10, 10, 10, 10, 10 }; // Speed of projectile
     float[] attackRanges = new float[] { 10f, 10f, 2f, 2f, 8f, 8f, 1.5f, 1.5f }; // Range of attack
-    int[] damages = new int[] { 5, 5, 10, 10, 5, 5, 10, 10 };  // Amount of damage
+    int[] damages = new int[] { 10, 10, 15, 15, 5, 5, 10, 10 };  // Amount of damage
     float[] attackIntervals = new float[] { 0.2f, 0.2f, 0.5f, 0.5f, .5f, .5f, .5f, .5f }; // Attack interval
-    float[] speeds = new float[] { 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f }; // Movement speed
-    int[] healths = new int[] { 200, 200, 200, 200, 40 , 60, 100, 100 }; // Health
+    float[] speeds = new float[] { 4.5f, 4.5f, 4.5f, 4.5f, 3.5f, 3.5f, 3.5f, 3.5f }; // Movement speed
+    int[] healths = new int[] { 200, 200, 200, 200, 20 , 30, 50, 60 }; // Health
 
     public bool npc;
     public bool ranged;
