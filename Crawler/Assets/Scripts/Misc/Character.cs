45b860c8af777098998d582573f53f7f0379e2f3
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-13 12:20:46 +0300

    Update Character.cs

--- Crawler/Assets/Scripts/Misc/Character.cs
+++ Crawler/Assets/Scripts/Misc/Character.cs
@@ -11,11 +11,11 @@ public class Character : Photon.MonoBehaviour {
     public bool[] npcs = new bool[] { false, false, false, false, true, true, true, true }; // Player or enemy
     public bool[] rangeds = new bool[] { true, true, false, false, true, true, false, false }; // Ranger or melee
     public float[] projectileSpeeds = new float[] { 10, 10, 10, 10, 10, 10, 10, 10 }; // Speed of projectile
-    public float[] attackRanges = new float[] { 10, 10, 3, 3, 10, 10, 3, 3 }; // Range of attack
-    public int[] damages = new int[] { 10, 10, 10, 10, 1, 1, 1, 1 };  // Amount of damage
-    public float[] attackIntervals = new float[] { 0.1f, 0.1f, 0.5f, 0.5f, 5f, 5f, 5f, 5f }; // Attack interval
+    public float[] attackRanges = new float[] { 10f, 10f, 2f, 2f, 8f, 8f, 1.5f, 1.5f }; // Range of attack
+    public int[] damages = new int[] { 10, 10, 10, 10, 5, 5, 10, 10 };  // Amount of damage
+    public float[] attackIntervals = new float[] { 0.1f, 0.1f, 0.5f, 0.5f, .5f, .5f, .5f, .5f }; // Attack interval
     public float[] speeds = new float[] { 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f, 3.5f }; // Movement speed
-    public int[] healths = new int[] { 200, 250, 300, 350, 100, 100, 100, 100 }; // Health
+    public int[] healths = new int[] { 200, 20, 200, 200, 20, 20, 50, 60 }; // Health
 
     public bool npc;
     public bool ranged;
