c531365a76836972a839d6fadec245109b015277
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-23 13:35:34 +0300

    Update PlayerFacingAt.cs

--- Crawler/Assets/Scripts/Player/PlayerFacingAt.cs
+++ Crawler/Assets/Scripts/Player/PlayerFacingAt.cs
@@ -2,37 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerFacingAt : Photon.MonoBehaviour
-{
+public class PlayerFacingAt : Photon.MonoBehaviour {
     public Camera myCam;
     public AudioListener myMic;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        if (photonView.isMine)
-        {
+    void Start() {
+        if(photonView.isMine) {
             myCam.enabled = true;
             myMic.enabled = true;
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (photonView.isMine)
-        {
-
-            // if (joystick input axis.x or axis.y > 0.1f){
-                //Vector2 dir = axis.x, axis.y;
-                //transform.right = dir;
-            //}
-
-            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-            Vector3 dir = Input.mousePosition - pos;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
+    void Update() {
+        if(photonView.isMine) {
+            Vector2 input = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
+            if(input.magnitude > 0.2f) {
+                transform.right = input;
+            } else {
+                Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+                Vector3 dir = Input.mousePosition - pos;
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
         }
     }
 }
