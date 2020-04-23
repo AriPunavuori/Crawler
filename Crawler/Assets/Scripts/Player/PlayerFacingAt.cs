d6969d3deb5500c9e89615cee13cdcb637be2ce5
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-23 16:45:26 +0300

    AwesomePawesomeCursor/Pointer

--- Crawler/Assets/Scripts/Player/PlayerFacingAt.cs
+++ Crawler/Assets/Scripts/Player/PlayerFacingAt.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFacingAt : Photon.MonoBehaviour {
+    public Texture2D crosshair;
+    public GameObject controllerPointer;
     public Camera myCam;
     public AudioListener myMic;
     Vector2 mousePos;
@@ -10,6 +12,7 @@ public class PlayerFacingAt : Photon.MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         if(photonView.isMine) {
+            Cursor.SetCursor(crosshair, Vector2.zero, CursorMode.Auto);
             myCam.enabled = true;
             myMic.enabled = true;
         }
@@ -20,6 +23,8 @@ public class PlayerFacingAt : Photon.MonoBehaviour {
         if(photonView.isMine) {
             mousePos = Input.mousePosition;
             if(mousePos != preMousePos) {
+                Cursor.visible = true;
+                controllerPointer.SetActive(false);
                 Vector2 pos = Camera.main.WorldToScreenPoint(transform.position);
                 Vector2 dir = (Vector2)Input.mousePosition - pos;
                 float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
@@ -27,6 +32,8 @@ public class PlayerFacingAt : Photon.MonoBehaviour {
             }
             Vector2 input = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"));
             if(input.magnitude > 0.2f) {
+                Cursor.visible = false;
+                controllerPointer.SetActive(true);
                 transform.right = input;
             }
             preMousePos = mousePos;
