4b8be7be646473c829c9194ba881c7af3319a6fe
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-26 16:32:36 +0300

    MenuAnimtesting

---
+++ Crawler/Assets/Scripts/Tweens/LeanTweenX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanTweenX : MonoBehaviour
+{
+    public RectTransform rectTrans;
+    public float timeOfEase;
+    public float whereToMove;
+    public GameObject[] objectsToEnable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeanTween.moveX(rectTrans, whereToMove, timeOfEase).setEaseInOutElastic();
+        Invoke("EnableObjects", timeOfEase);
+    }
+    void EnableObjects() {
+        foreach(var o in objectsToEnable) {
+            o.SetActive(true);
+        }
+    }
+}
---
+++ Crawler/Assets/Scripts/Tweens/LeanTweenY.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanTweenY : MonoBehaviour
+{
+    public RectTransform rectTrans;
+    public float timeOfEase;
+    public float whereToMove;
+    public GameObject[] objectsToEnable;
+    // Start is called before the first frame update
+    void Start() {
+        LeanTween.moveY(rectTrans, whereToMove, timeOfEase).setEaseInOutElastic();
+        Invoke("EnableObjects", timeOfEase);
+    }
+    void EnableObjects() {
+        foreach(var o in objectsToEnable) {
+            o.SetActive(true);
+        }
+    }
+}
