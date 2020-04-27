00659eee73f5cbf622276dea155529f0e38dc49d
Ari Punavuori <ari.punavuori@gmail.com>
2020-04-27 13:19:31 +0300

    Button Juicer

--- Crawler/Assets/Scripts/Tweens/ButtonJuicer.cs
+++ Crawler/Assets/Scripts/Tweens/ButtonJuicer.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonJuicer : MonoBehaviour {
+    public float factor = 1.05f;
+    Shadow shadow;
+    Vector2 originalEffectDistance;
+    private void Awake() {
+        shadow = GetComponent<Shadow>();
+        if(shadow != null) {
+            originalEffectDistance.x = shadow.effectDistance.x;
+            originalEffectDistance.y = shadow.effectDistance.y;
+        }
+    }
+
     public void OnButtonHoverEnter(RectTransform rect) {
-        LeanTween.scale(rect, Vector3.one * 1.1f, .05f);
+        LeanTween.scale(rect, Vector3.one * factor, .05f);
+        if(shadow != null) {
+            shadow.effectDistance = originalEffectDistance * factor * 1.5f;
+        }
         AudioFW.Play("ButtonSwitch");
     }
 
     public void OnButtonHoverExit(RectTransform rect) {
         LeanTween.scale(rect, Vector3.one, .05f);
+        if(shadow != null) {
+            shadow.effectDistance = originalEffectDistance;
+        }
         AudioFW.Play("ButtonSwitch");
     }
 
