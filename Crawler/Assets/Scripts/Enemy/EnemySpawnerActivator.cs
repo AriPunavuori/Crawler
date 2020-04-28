43e948c2b9969ed77d07fe91e07e10a8e9bb9ac2
JuhisRasis <47595899+JuhisRasis@users.noreply.github.com>
2020-04-28 15:59:44 +0300

    EnemySpawners based on player amount

---
+++ Crawler/Assets/Scripts/Enemy/EnemySpawnerActivator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerActivator : MonoBehaviour
+{
+    GameObject[] players;
+    public GameObject EnemySpawnerPlayers2;
+    public GameObject EnemySpawnerPlayers3;
+    public GameObject EnemySpawnerPlayers4;
+
+    void Start()
+    {
+        Invoke("ActivateSpawners", 2f);
+    }
+
+    void ActivateSpawners()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log(players.Length);
+        if(players.Length > 1)
+        {
+            EnemySpawnerPlayers2.gameObject.SetActive(true);
+            if (players.Length > 2)
+            {
+                Debug.Log("a");
+
+                EnemySpawnerPlayers3.gameObject.SetActive(true);
+
+                if (players.Length > 3)
+                {
+                    EnemySpawnerPlayers4.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
