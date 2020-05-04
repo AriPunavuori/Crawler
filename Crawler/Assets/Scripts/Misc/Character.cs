c167213c229cb65a22616875c1337a3bb76d691c
Emil Virolainen <virolainen.emil@gmail.com>
2020-05-04 16:06:18 +0300

    UI-päivitys + lisää ääniä

--- Crawler/Assets/Scripts/Misc/Character.cs
+++ Crawler/Assets/Scripts/Misc/Character.cs
@@ -6,46 +6,47 @@ public enum EntityType { Hero0, Hero1, Hero2, Hero3, Enemy0, Enemy1, Enemy2, Ene
 
 public class Character : Photon.MonoBehaviour {
 
-    public EntityType characterType;
+	public EntityType characterType;
 
-    // Four first of the values are for heroes the rest for enemies
-    bool[] rangeds = new bool[] { true, false, true, false, true, true, false, false }; // Ranger or melee
-    float[] projectileSpeeds = new float[] { 15f, 0f, 7.5f, 0f, 6.5f, 6.5f, 10f, 10f }; // Speed of projectile
-    float[] attackRanges = new float[] { 7.5f, 1.5f, 10f, 1.5f, 6.5f, 6.5f, 1.5f, 1.5f }; // Range of attack
-    int[] damages = new int[] { 10, 20, 15, 25, 5, 15, 5, 50 };  // Amount of damage
-    float[] attackIntervals = new float[] { 0.2f, 0.4f, 0.3f, 0.45f, .5f, .75f, .5f, 1.5f }; // Attack interval
-    float[] speeds = new float[] { 4.5f, 4.5f, 4.5f, 4.5f, 3.5f, 0.5f, 5f, 4.5f }; // Movement speed
-    int[] healths = new int[] { 150, 200, 150, 250, 20 , 40, 10, 80 }; // Health
-    float[] specialCooldowns = new float[] { 3.0f, 10.0f, 7.0f, 10.0f, 0f, 0f, 0f, 0f }; // Special cooldowns
+	// Four first of the values are for heroes the rest for enemies
+	bool[] rangeds = new bool[] { true, false, true, false, true, true, false, false }; // Ranger or melee
+	float[] projectileSpeeds = new float[] { 15f, 0f, 7.5f, 0f, 6.5f, 6.5f, 10f, 10f }; // Speed of projectile
+	float[] attackRanges = new float[] { 7.5f, 1.5f, 10f, 1.5f, 6.5f, 6.5f, 1.5f, 1.5f }; // Range of attack
+	int[] damages = new int[] { 10, 20, 15, 25, 5, 15, 5, 50 };  // Amount of damage
+	float[] attackIntervals = new float[] { 0.2f, 0.4f, 0.3f, 0.45f, .5f, .75f, .5f, 1.5f }; // Attack interval
+	float[] speeds = new float[] { 4.5f, 4.5f, 4.5f, 4.5f, 3.5f, 0.5f, 5f, 4.5f }; // Movement speed
+	int[] healths = new int[] { 150, 200, 150, 250, 20, 40, 10, 80 }; // Health
+	float[] specialCooldowns = new float[] { 3.0f, 10.0f, 7.0f, 10.0f, 0f, 0f, 0f, 0f }; // Special cooldowns
 
-    public bool ranged;
-    public float projectileSpeed;
-    public float attackRange;
-    public int damage;
-    public float attackInterval;
-    public float speed;
-    public int health;
-    public float specialCooldown;
-   
-    public float attackTime;
-    public int weaponLevel;
+	public bool ranged;
+	public float projectileSpeed;
+	public float attackRange;
+	public int damage;
+	public float attackInterval;
+	public float speed;
+	public int health;
+	public float specialCooldown;
 
-    public GameObject projectileSpawn;
-    public GameObject projectilePrefab;
+	public float attackTime;
+	public int speedLevel;
+	public int weaponLevel;
 
-    public Vector2 movement;
+	public GameObject projectileSpawn;
+	public GameObject projectilePrefab;
 
-    public int CheckCharacterHealt(EntityType et) {
-        return healths[(int)et];
-    }
-    public void SetCharacterAttributes() {
-        ranged = rangeds[(int)characterType];
-        projectileSpeed = projectileSpeeds[(int)characterType];
-        attackRange = attackRanges[(int)characterType];
-        damage = damages[(int)characterType];
-        attackInterval = attackIntervals[(int)characterType];
-        speed = speeds[(int)characterType];
-        health = healths[(int)characterType];
-        specialCooldown = specialCooldowns[(int)characterType];
-    }
+	public Vector2 movement;
+
+	public int CheckCharacterHealt(EntityType et) {
+		return healths[(int)et];
+	}
+	public void SetCharacterAttributes() {
+		ranged = rangeds[(int)characterType];
+		projectileSpeed = projectileSpeeds[(int)characterType];
+		attackRange = attackRanges[(int)characterType];
+		damage = damages[(int)characterType];
+		attackInterval = attackIntervals[(int)characterType];
+		speed = speeds[(int)characterType];
+		health = healths[(int)characterType];
+		specialCooldown = specialCooldowns[(int)characterType];
+	}
 }
