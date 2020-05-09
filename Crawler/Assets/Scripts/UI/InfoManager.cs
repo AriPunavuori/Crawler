a8de1ce1bdb525ba7992c4c1689b2e9192705fdb
Ari Punavuori <ari.punavuori@gmail.com>
2020-05-09 09:35:51 +0300

    MenuUI Mehuistus™

--- Crawler/Assets/Scripts/UI/InfoManager.cs
+++ Crawler/Assets/Scripts/UI/InfoManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -16,52 +15,71 @@ public class InfoManager : MonoBehaviour {
     public GameObject heroImageCard;
     Character character;
     bool selected;
+    Action complete;
+    bool movingOut;
+    int queue;
 
     private void Awake() {
         character = GameObject.Find("BaseHealth").GetComponent<Character>();
+        complete = OnComplete;
+    }
+    private void Update() {
+        if(queue > -1)
+            Show(queue);
     }
 
     public void Show(int hero) {
-        if(!selected) {
-
-            frameImage.color = heroColors[hero];
-            heroImage = heroImages[hero];
-            Image SetheroImageCard = heroImageCard.GetComponent(typeof(Image)) as Image;
-            SetheroImageCard.sprite = heroImage;
-            for(int i = 0; i < textFields.Length; i++) {
-                textFields[i].text = character.GetCharacterData(i, hero);
+        if(!movingOut) {
+            if(!selected) {
+                frameImage.color = heroColors[hero];
+                heroImage = heroImages[hero];
+                Image SetheroImageCard = heroImageCard.GetComponent(typeof(Image)) as Image;
+                SetheroImageCard.sprite = heroImage;
+                for(int i = 0; i < textFields.Length; i++) {
+                    textFields[i].text = character.GetCharacterData(i, hero);
+                }
+                ShowCard();
             }
-            ShowCard();
-        }
+        } else
+            queue = hero;
         HideTitle();
     }
 
     public void Hide(int i) {
-        ShowTitle();
-        HideCard();
+        queue = -1;
+        if(!movingOut) {
+            movingOut = true;
+            ShowTitle();
+            HideCard();
+        }
     }
 
     public void ShowCard() {
         LeanTween.cancel(infoCardRectTransform);
-        LeanTween.move(infoCardRectTransform, Vector3.zero, .5f).setEaseOutCirc();
+        LeanTween.move(infoCardRectTransform, Vector3.zero, .5f).setEaseOutCubic();
     }
 
     public void HideCard() {
         LeanTween.cancel(infoCardRectTransform);
-        LeanTween.move(infoCardRectTransform, Vector3.right * 2500, .5f).setEaseOutCirc();
+        LeanTween.move(infoCardRectTransform, Vector3.right * 2500, .5f).setEaseInCubic().setOnComplete(complete);
     }
 
     public void ShowTitle() {
         LeanTween.cancel(title);
-        LeanTween.move(title, Vector3.zero, .5f).setEaseOutCirc();
+        LeanTween.move(title, Vector3.zero, .5f).setEaseInCubic();
     }
 
     public void HideTitle() {
         LeanTween.cancel(title);
-        LeanTween.move(title, Vector3.left * 2500, .5f).setEaseOutCirc();
+        LeanTween.move(title, Vector3.left * 2500, .5f).setEaseOutCubic();
     }
 
     public void SelectedCharacter() {
         selected = true;
     }
+
+    void OnComplete() {
+        movingOut = false;
+    }
+
 }
